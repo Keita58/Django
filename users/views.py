018ae49eb5456b7e3eee04302b72df2da761ce29
Keita58 <marc-s-26@hotmail.com>
2025-03-29 17:37:31 +0100

    Projecte acabat

--- users/views.py
+++ users/views.py
@@ -1,14 +1,43 @@
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+
+from Enquestes.models import Resposte, Questionari, Pregunte
 
 
 # Create your views here.
 class LoginView(View):
     def get(self, request):
         if request.user.is_authenticated:
-            return render(request, 'Inici.html')
+            if request.user.groups.filter(name='Professor').exists():
+                respostes = Resposte.objects.filter(tancar=0)
+                print(respostes)
+                preguntes = []
+                for i in range(len(respostes)):
+                    aux = get_object_or_404(Pregunte, idPreguntes=respostes[i].idPregunta.idPreguntes)
+                    if aux not in preguntes:
+                        preguntes.append(aux)
+
+                questionaris = []
+                for i in range(len(preguntes)):
+                    aux = get_object_or_404(Questionari, idQuestionaris = preguntes[i].questionari.idQuestionaris)
+                    if aux not in questionaris:
+                        questionaris.append(aux)
+
+                context = {
+                    "questionaris": questionaris,
+                    "respostes": respostes,
+                    "preguntes": preguntes
+                }
+                return render(request, 'Inici.html', context)
+            else:
+                context = {
+                    "questionaris": Questionari.objects.all(),
+                    "respostes": Resposte.objects.all(),
+                    "preguntes": Pregunte.objects.all()
+                }
+                return render(request, 'Inici.html', context)
         else:
             return render(request, 'login.html')
 
@@ -22,7 +51,7 @@ class LoginView(View):
         if user is not None:
             # Mètode de django que obre una sessió d'usuari
             login(request, user)
-            return HttpResponse(content='Success')
+            return redirect('')
         # Fem aquest return per permetre tornar a fer login
         return self.get(request)
 
